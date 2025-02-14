e99328876a40d4538e4be748437a60f62bafcace
Philip <m.p.o.andersson@gmail.com>
2025-02-14 11:12:52 +0100

    added delete person

--- 06-lmdb/src/resources/person/person.controller.ts
+++ 06-lmdb/src/resources/person/person.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Debug from "debug";
 import mongoose from "mongoose";
 import { Person } from "./person.model";
+import { Movie } from "../movie/movie.model";
 const debug = Debug("lmdb:person.controller");
 
 /**
@@ -44,6 +45,14 @@ export const show = async (req: Request, res: Response) => {
 		// Find a single person
 		const person = await Person.findById(personId);
 
+		const acted_in = await Movie
+			.find({ actors: personId })
+			.select(["title", "release_year"]);
+
+		const directed_by = await Movie
+			.find({ director: personId })
+			.select(["title", "release_year"]);
+		
 		// If no person was found, respond with 404
 		if (!person) {
 			res.status(404).send({
@@ -57,7 +66,11 @@ export const show = async (req: Request, res: Response) => {
 
 		res.send({
 			status: "success",
-			data: person,
+			data: {
+				person,
+				acted_in,
+				directed_by,
+			}
 		});
 
 	} catch (err) {
@@ -98,4 +111,107 @@ export const store = async (req: Request, res: Response) => {
 			message: "Error thrown when creating person",
 		});
 	}
+}
+
+export const update = async (req: Request, res: Response) => {
+	const personId = req.params.personId;
+
+	if(!mongoose.isValidObjectId(personId)){
+		res.status(400).send({ status: "fail", data: {
+			message: "That is not a vaild ID"
+		}});
+		return;
+	}
+
+	try {
+		const updatedPerson = await Movie.findByIdAndUpdate(personId , req.body, { 
+			new: true, 
+			runValidators: true 
+		});
+
+		if (!updatedPerson) {
+			res.status(404).send({
+				status: "fail",
+				data: {
+					message: "Person Not Found",
+				},
+			});
+			return;
+		}
+
+		res.status(200).send({status: "success", data: updatedPerson});
+
+	} catch (error) {
+
+		if(error instanceof mongoose.Error.ValidationError){
+			debug("Validation failed when updating person %o: %O", req.body, error);
+			res.status(400).send({
+				status: "fail",
+				data: error.errors,
+			});
+			return
+		}
+
+		debug("Error thrown when creating movie %o: %O", req.body, error);
+		res.status(500).send({
+			status: "error",
+			message: "Error thrown when updating person",
+		});
+	}	
+}
+
+export const destroy = async (req: Request, res: Response) => {
+	const personId = req.params.personId;
+
+	if(!mongoose.isValidObjectId(personId)){
+		res.status(400).send({ status: "fail", data: {
+			message: "That is not a vaild ID"
+		}});
+		return;
+	}
+
+	try {
+
+		await Movie.updateMany(
+			{director: personId},
+			{director: null}
+		);
+
+		await Movie.updateMany(
+			{actors: personId},
+			{$pull: {actors: personId}}
+		);
+		const deletedPerson = await Movie.findByIdAndDelete(personId);
+
+		if (!deletedPerson) {
+			res.status(404).send({
+				status: "fail",
+				data: {
+					message: "Movie Not Found",
+				},
+			});
+			return;
+		}
+
+		res.status(204).send(deletedPerson);
+
+	} catch (error) {
+
+		if(error instanceof mongoose.Error.ValidationError){
+			debug("Validation failed when deleting person %o: %O", req.body, error);
+			res.status(400).send({
+				status: "fail",
+				data: error.errors,
+			});
+			return
+		}
+
+		debug("Error thrown when deleting person %o: %O", req.body, error);
+		res.status(500).send({
+			status: "error",
+			message: "Error thrown when deleting person",
+		});
+		
+	}
+
 }
